fb9b1aa3d6fe5c316879cd7c16883d90b1a66d0d
Maxime <titouf-72@hotmail.fr>
2019-02-08 15:34:42 +0100

    Fin de séance, avancée dans les aides, amélioration de l'arnorescence et ajout d'aides

--- Game/Helps/Helper.rb
+++ Game/Helps/Helper.rb
@@ -1,12 +1,17 @@
 #Main of the help, when you need a help, let's call Helper to help you
 
-require File.dirname(__FILE__) + "/GrassHelper"
+require File.dirname(__FILE__) + "/RowsOrColumnsHelpers/RowsAndColumnsGrassHelper"
+require File.dirname(__FILE__) + "/RowsOrColumnsHelpers/RowsAndColumnsTentsHelper"
+
 
 class Helper
   def Helper.help( game )
     game.beginGuess
 
-    GrassHelper.help(game)
+    CellDontTouchTreeHelper.help(game)#1
+    
+    RowsAndColumnsGrassHelper.help(game) #3
+    RowsAndColumnsTentsHelper.help(game) #4
 
     game.removeGuess
   end
