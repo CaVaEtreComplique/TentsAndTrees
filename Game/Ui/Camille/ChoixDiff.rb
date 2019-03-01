2af7b05bf5c96d6d11c61082ec57064e9ff753e8
Vaidie Camille <s156388@ic2s114-19.univ-lemans.fr>
2019-03-01 15:56:03 +0100

    Ajout variable pour maintenabilité sur choixdiff,fenetrePrinc,Modedejeu,Parametre

--- Game/Ui/Camille/ChoixDiff.rb
+++ Game/Ui/Camille/ChoixDiff.rb
@@ -12,35 +12,40 @@ end
 class ChoixDiff < Screen
   def initialize(manager)
     super(manager.win)
+    screen=Gdk::Screen.default
     @pad =10
+    @widthTitre=screen.width*0.10
+    @heightTitre=screen.height*0.08
+    @widthText=screen.width*0.1
+    @heightText=screen.height*0.04
     screen=Gdk::Screen.default
     #menuV=box verticale
     @gtkObject= Gtk::Table.new(3,3)
     @menu=Gtk::Box.new(:vertical, 100)
     @gtkObject.attach(@menu,1,2,1,2)
 
-    titre=Text.new("Choix du niveau",screen.width*0.10,screen.height*0.08)
+    titre=Text.new("Choix du niveau",@widthTitre,@heightTitre)
     @menu.pack_start(titre.gtkObject ,expand: false, fill: true, padding: @pad)
 
-    facile=Text.new("Facile",screen.width*0.1,screen.height*0.04)
+    facile=Text.new("Facile",@widthText,@heightText)
 		 @menu.pack_start(facile.gtkObject ,expand: false, fill: true, padding: @pad)
 		facile.onClick{
 			 session=Session.new(:timeattack, :easy)
 			 manager.runGameSession(session)
 		}
-    moyen=Text.new("Moyen",screen.width*0.1,screen.height*0.04)
+    moyen=Text.new("Moyen",@widthText,@heightText)
 		 @menu.pack_start(moyen.gtkObject ,expand: false, fill: true, padding: @pad)
 		moyen.onClick{
 			 session=Session.new(:timeattack, :medium)
 			 manager.runGameSession(session)
 		}
-    diff=Text.new("Difficile",screen.width*0.1,screen.height*0.04)
+    diff=Text.new("Difficile",@widthText,@heightText)
 		 @menu.pack_start(diff.gtkObject ,expand: false, fill: true, padding: @pad)
 		diff.onClick{
 			 session=Session.new(:timeattack, :hard)
 			 manager.runGameSession(session)
 		}
-    retour=Text.new("retour",screen.width*0.1,screen.height*0.04)
+    retour=Text.new("retour",@widthText,@heightText)
     @menu.pack_start(retour.gtkObject ,expand: false, fill: true, padding: 10)
     retour.onClick{
         manager.modeScreen.applyOn(@parent)
--- Game/Ui/Camille/FenetrePrinc.rb
+++ Game/Ui/Camille/FenetrePrinc.rb
@@ -6,9 +6,14 @@ require File.dirname(__FILE__) + "/../Buttons/Text"
 
 class FenetrePrinc < Screen
   def initialize(manager)
-    @pad=50
     super(manager.win)
     screen=Gdk::Screen.default
+    @pad=50
+    @widthTitre=screen.width*0.10
+    @heightTitre=screen.height*0.08
+    @widthText=screen.width*0.15
+    @heightText=screen.height*0.06
+
     #Menu principal
     @gtkObject= Gtk::Table.new(3,3)
 
@@ -16,24 +21,25 @@ class FenetrePrinc < Screen
     menuV=Gtk::Box.new(:vertical)
     @gtkObject.attach(menuV,1,2,0,1)
     #Premiere case : titre
-    titre=Text.new("dEs ArbrEs Et dEs tEntEs",screen.width*0.10,screen.height*0.08)
+    titre=Text.new("dEs ArbrEs Et dEs tEntEs",@widthTitre,@heightTitre)
     menuV.pack_start(titre.gtkObject ,expand: false, fill: true, padding: @pad)
 
     #Bontons de menu
-    jouer=Text.new("jouer",screen.width*0.15,screen.height*0.06)
+    jouer=Text.new("jouer",@widthText,@heightText)
     menuV.pack_start(jouer.gtkObject ,expand: false, fill: true, padding: @pad)
     jouer.onClick{
         manager.modeScreen.applyOn(@parent)
     }
 
-    parametre=Text.new("parametres",screen.width*0.15,screen.height*0.06)
+    parametre=Text.new("parametres",@widthText,@heightText)
     menuV.pack_start(parametre.gtkObject ,expand: false, fill: true, padding: @pad)
     parametre.onClick{
         manager.paramScreen.applyOn(@parent)
     }
-    regle=Text.new("regle",screen.width*0.15,screen.height*0.06)
+    regle=Text.new("regle",@widthText,@heightText)
     menuV.pack_start(regle.gtkObject ,expand: false, fill: true, padding: @pad)
-    meilleurScore=Text.new("meilleur Score",screen.width*0.15,screen.height*0.06)
+
+    meilleurScore=Text.new("meilleur Score",@widthText,@heightText)
     menuV.pack_start(meilleurScore.gtkObject ,expand: false, fill: true, padding: @pad)
 
     @gtkObject.attach(Gtk::Image.new(pixbuf: @buffer),0,3,0,3)
