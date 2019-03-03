9260e21a7597e819eb7efa9d2154e265b61a4f0b
root <root@localhost.localdomain>
2019-03-03 15:10:34 -0500

    Second correction (partielle) de l'arborescence Help (+un bug mineur a voir au prochain cour)

--- Game/Helps/HelpClasses/HelpsCellsAndRowsOrColumns/HelpsCellsAndColumns/HelpCellsAndColumns.rb
+++ Game/Helps/HelpClasses/HelpsCellsAndRowsOrColumns/HelpsCellsAndColumns/HelpCellsAndColumns.rb
@@ -1,8 +1,11 @@
+
+
 require File.dirname(__FILE__) + "/../HelpCellsAndRowsOrColumns"
 
 class HelpCellsAndColumns < HelpCellsAndRowsOrColumns
-  private_class_methode :new  #abstract
-  def initialize(column, cell)
-    super(column, cell)
+  #private_class_methode :new  #abstract
+
+  def initialize(cell, column)
+    super(cell, column)
   end
 end
--- Game/Helps/HelpClasses/HelpsCellsAndRowsOrColumns/HelpsCellsAndColumns/HelpsAllPossibilitiesGiveItColumns/HelpAllPossibilitiesGiveItColumn.rb
+++ Game/Helps/HelpClasses/HelpsCellsAndRowsOrColumns/HelpsCellsAndColumns/HelpsAllPossibilitiesGiveItColumns/HelpAllPossibilitiesGiveItColumn.rb
@@ -3,11 +3,13 @@ require File.dirname(__FILE__) + "/../HelpCellsAndColumns"
 #Help for when all the possibilities of the row give a cell with a state
 
 class HelpAllPossibilitiesGiveItColumn < HelpCellsAndRowsOrColumns
-  def initialize(cell, row, state)
-    super(cell, row)
+  def initialize(cell, column, state)
+    super(cell, column)
     @state = state
+    puts "Test " + @state.to_s
   end
+
   def getText()
-    return "Toutes les possibilités de la rangée "+ row +" font que la case a l'état "+state
+    @helpText = "Toutes les possibilités de la rangée " + @row.to_s + " font que la case a l'état " +  @state.to_s + " Test"
   end
 end
