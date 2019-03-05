c1977df79afe464cf74c1474cd7d4013fb50adeb
Tourpe Florian <s160639@ic2s120-07.univ-lemans.fr>
2019-03-05 10:01:25 +0100

    Ajoute une méthode de compression de grille

---
+++ Game/Helps/CompresseGrid.rb
@@ -0,0 +1,26 @@
+# Compresse the grid into 3 array whitch containe tent, tree and grass
+
+
+class CompresseGrid
+  def CompresseGrid.compresse(game)
+
+    tree = Array.new
+    tent = Array.new 
+    grass = Array.new
+
+    (0...game.nRow).each do |i|
+      (0...game.nCol).each do |j|
+        cell = game.cellAt(i, j)
+        if cell.isATent?
+          tent.push(cell)
+        elsif cell.isATree?
+          tree.push(cell)
+        elsif cell.isAGrass?
+          grass.push(cell)
+        end
+      end
+    end
+
+    return [tree, tent, grass]
+  end
+end
