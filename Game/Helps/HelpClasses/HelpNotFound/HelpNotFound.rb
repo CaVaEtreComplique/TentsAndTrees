42ced6b0aa9b2ab169fcb8798bc1133b08716212
Lucien <lbouyeur@gmail.com>
2019-03-08 17:49:59 +0100

    RDoc générée

--- Game/Helps/HelpClasses/HelpNotFound/HelpNotFound.rb
+++ Game/Helps/HelpClasses/HelpNotFound/HelpNotFound.rb
@@ -1,12 +1,21 @@
 require File.dirname(__FILE__) + "/../Help"
 
+##
+# The HelpNotFound class is inherited from the Help class. This class is
+# instanciated when no help can be found.
 class HelpNotFound < Help
   #Class instanciated when any help has been found
   public_class_method :new #instanciable
-  def helpFound?() #redifinition of the method, no help found, return false
+
+  # This is a redefinition of the Help.helpFound? method. It returns false
+  # because no help has been found.
+  def helpFound?()
     return false
   end
-  def getText() #returns the text of the help, saying that there is no help
+
+  # This method is a redefinition of the Help.getText method. It returns a text
+  # saying that no help has been found.
+  def getText()
     return "Aucune aide trouvée"
   end
 end
