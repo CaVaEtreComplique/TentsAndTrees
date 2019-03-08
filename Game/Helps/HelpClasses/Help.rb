7f4a346581830175800b043197a8a1c9bca0baef
Lucien <lbouyeur@gmail.com>
2019-03-08 16:03:50 +0100

    RDoc générée

--- Game/Helps/HelpClasses/Help.rb
+++ Game/Helps/HelpClasses/Help.rb
@@ -1,27 +1,42 @@
-#class Help, which contain a String text to explain the help
-#this class is abstract
 require_all(__FILE__)
+##
+#The help class is an abstract class which contains a String to explain the
+#help. It contains the methods helpFound? and getText and they act on the help
+#text.
 class Help
-  #parent of all helps, used any time when you ask a help
+  # :nodoc:
+  #Parent of all helps, used any time when you ask for help.
   private_class_method :new  #abstract class
 
+  # :startdoc:
+  # The class' constructor.
   def initialize()
-    #initialize the help text as what it need to be
+    #Initializes the help text as what it needs to be.
     @helpText = self.getText
   end
 
+  # :nodoc:
   attr_reader :helpText
+  # :startdoc:
 
+  # This tries to find the necessary help.
+  # ===== Returns
+  # Returns true if help has been found, else it returns false.
+  # --------------
   def helpFound?()
-    #return true if a help has been found, else return false
     return true
   end
 
+  # This method fetches the help text.
+  # ===== Returns
+  # Returns the help text.
+  # ---------
   def getText()
-    #retrun the text for the help
     return "Txt not Available"
   end
 
+  # This method prints the help text.
+  # -----------
   def to_s
     return @helpText + "\n"
   end
