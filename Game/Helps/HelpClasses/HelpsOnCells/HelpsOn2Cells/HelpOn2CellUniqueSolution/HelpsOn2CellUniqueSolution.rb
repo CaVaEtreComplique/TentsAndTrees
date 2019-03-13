f8b5829b28383060d2d37c0f051e564664522b42
root <root@localhost.localdomain>
2019-03-13 17:24:24 -0400

    Dernière aides : liage des tentes et arbre.

---
+++ Game/Helps/HelpClasses/HelpsOnCells/HelpsOn2Cells/HelpOn2CellUniqueSolution/HelpsOn2CellUniqueSolution.rb
@@ -0,0 +1,14 @@
+require File.dirname(__FILE__) + "/../HelpOn2Cells"
+
+class HelpsOn2CellUniqueSolution < HelpOn2Cells
+  #a class for helping when a cell is link to a tree because of only one posibility
+  public_class_method :new #instanciable
+  def initialize(cellTent, cellTree)
+    super(cellTent, cellTree)
+  end
+
+  def getText()
+    #returns the text of the help, saying that a cell touch a class and is some grass
+    return "La case " + @cell.to_s + " est une tent car elle est la solution unique de l'arbre en " + @cell2.to_s
+  end
+end
