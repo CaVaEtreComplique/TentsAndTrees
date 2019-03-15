62cd5a3d73dc9006c808a8a008597fc7daa3081a
Lucien <lbouyeur@gmail.com>
2019-03-15 10:42:38 +0100

    Rdoc générée

--- Game/Helps/HelpClasses/HelpsCellsAndRowsOrColumns/HelpsCellsAndColumns/HelpCellsAndColumns.rb
+++ Game/Helps/HelpClasses/HelpsCellsAndRowsOrColumns/HelpsCellsAndColumns/HelpCellsAndColumns.rb
@@ -9,10 +9,17 @@
 
 require File.dirname(__FILE__) + "/../HelpCellsAndRowsOrColumns"
 
+##
+# The HelpCellsAndColumns class is an abstract class inherited from the abstract
+# class HelpCellsAndRowsOrColumns. This abstract class will determine a model
+# for every help class that needs to know a cell and a column. The only method
+# this class knows is its initialization method.
 class HelpCellsAndColumns < HelpCellsAndRowsOrColumns
   #abstract class
 
+##
+# This method only does what the initialize method in HelpCellsAndRowsOrColumns
+# does, but with a column instead of a row XOR a column.
   def initialize(cell, column)
     super(cell, column)
   end
-end
