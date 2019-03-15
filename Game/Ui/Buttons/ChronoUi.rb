f35298c3da6d365ace3ec0a4943a07a3e87e2eab
Lucien <lbouyeur@gmail.com>
2019-03-15 16:02:01 +0100

    RDoc générée

--- Game/Ui/Buttons/ChronoUi.rb
+++ Game/Ui/Buttons/ChronoUi.rb
@@ -7,20 +7,42 @@
 
 require File.dirname(__FILE__) + "/Button"
 
-
+##
+# ==== Presentation
+#
+# The ChronoUI class is inherited from the Button class and manages the in-game
+# chronometer.
+#
+# ==== Methods
+#
+# This class knows three methods : initialize, updateLabel (both
+# inherited from the Button class) and the parce method. These methods are
+# described further down.
+#
+# ==== Variable
+#
+# The only variable this class needs is the time value.
 class ChronoUi < Button
 
+	#:nodoc:
 	attr_reader :time
+	#:startdoc:
 
+	# The initialization method creates a new button with specific values for the
+	# chronometer's creation.
 	def initialize(time)
     super(:horizontal,"%02dh %02dm %02ds" % parce(time))
 	end
 
-
+	# The updateLabel method updates the chronometer button's label in order to
+	# actualize it at each time change.
   def updateLabel(time)
 	  super("%02dh %02dm %02ds" % parce(time))
   end
 
+	# The parce method is needed for the initialization and for the label update.
+	# It gives the time a value that is easily read by humans. By doing so, it
+	# can be displayed in the game.
 	def parce(time)
 	  s, m = time % 60, time / 60
 	  h = m / 60
