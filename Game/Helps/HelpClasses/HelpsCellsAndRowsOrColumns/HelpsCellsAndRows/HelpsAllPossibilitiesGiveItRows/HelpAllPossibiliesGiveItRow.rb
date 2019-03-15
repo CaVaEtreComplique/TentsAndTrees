432f763e86d4dbc4b7e1bb345c94e329b2e5732b
Lucien <lbouyeur@gmail.com>
2019-03-15 11:29:53 +0100

    RDoc Générée

--- Game/Helps/HelpClasses/HelpsCellsAndRowsOrColumns/HelpsCellsAndRows/HelpsAllPossibilitiesGiveItRows/HelpAllPossibiliesGiveItRow.rb
+++ Game/Helps/HelpClasses/HelpsCellsAndRowsOrColumns/HelpsCellsAndRows/HelpsAllPossibilitiesGiveItRows/HelpAllPossibiliesGiveItRow.rb
@@ -5,20 +5,34 @@
 # @Last modified by:   Maxime
 # @Last modified time: 15-Mar-2019
 
-
-
 require File.dirname(__FILE__) + "/../HelpCellsAndRows"
 
+##
+# The HelpAllPossibilitiesGiveItRow class is inherited from the
+# HelpCellsAndRows class and helps when all the possibilities of the row
+# can determine a cell's state. This class knows its initialization method and
+# the getText method that sends text to the player when he/she needs help.
 class HelpAllPossibilitiesGiveItRow < HelpCellsAndRows
-  #Help for when all the possibilities of the row give a cell a certain state
-  public_class_method :new #instanciable
+
+   #:nodoc:
+  public_class_method :new
+  #:startdoc:
+
+ ##
+ # The initialize method gives to the state variable a value from the method's
+ # parameters and initializes the cell and the row in parameters with the
+ # method from HelpCellsAndColumns.
   def initialize(cell, row, state)
     @state = state
     super(cell, row)
   end
 
+  ##
+  # The getText method sends the help text the player will see.
+  # It returns the helpText variable, here the text that will be returned says
+  # that all the row's possibilities give only one possible option for a
+  # given cell.
   def getText()
-    #returns the text of the help, saying that all the possibilitieses of a row give the state of the cell
     @helpText = "Toutes les possibilités de la rangée " + @row.to_s + " font que la case " + @cell.to_s + " est a l'état " + @state.to_s
   end
 end
