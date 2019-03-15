2f3ebd3662c78038f331a2d0d0184fa55b551957
Sckylle <florian.tourpe@gmail.com>
2019-03-15 12:17:20 +0100

    Helper

--- Game/Helps/Helpers/Helper.rb
+++ Game/Helps/Helpers/Helper.rb
@@ -16,7 +16,6 @@ require File.dirname(__FILE__) + "/RowsOrColumnsHelpers/RowsAndColumnsFindGrass"
 require File.dirname(__FILE__) + "/CellsHelpers/CellDontTouchTreeHelper"
 require File.dirname(__FILE__) + "/CellsHelpers/CellTouchATentHelper"
 require File.dirname(__FILE__) + "/RecursivHelpers/AllTreesHaveTentsHelper"
-require File.dirname(__FILE__) + "/RecursivHelpers/AllTreesHaveTentsHelper2"
 require File.dirname(__FILE__) + "/CellsHelpers/CellWhiteOverlap"
 require File.dirname(__FILE__) + "/../GameDecorated"
 
@@ -40,14 +39,14 @@ class Helper < FictivHelper
     #initialize with all helpers
     @helps = Array.new
 
-    @helps.push(CellDontTouchTreeHelper.new)    # Help when a cell dont touch a tree, it will be a grass
-    @helps.push(CellTouchATentHelper.new)       # Fill the cells near a Tent with grass
-    @helps.push(RowsAndColumnsGrassHelper.new)  #fill a row/column whith grass when it has all this tents
-    @helps.push(RowsAndColumnsTentsHelper.new)  #fill a row/column whith tents when it has all this grass
-    @helps.push(RowsAndColumnsFindGrass.new)    # RowsAndColumnsFindGrass help you to find grass in a row or columns
-    @helps.push(RowsAndColumnsFindTent.new)     # RowsAndColumnsFindTent help you to find Tent in a row or columns
-    @helps.push(CellWhiteOverlap.new)           # Help when all possibilities give a grass cell
-    @helps.push(AllTreesHaveTentsHelper2.new)   #  Place a tent for a tree that don't has his tent
+    # @helps.push(CellTouchATentHelper.new)       # Fill the cells near a Tent with grass
+    # @helps.push(RowsAndColumnsTentsHelper.new)  # Fill a row/column whith tents when it has all this grass
+    # @helps.push(RowsAndColumnsGrassHelper.new)  # Fill a row/column whith grass when it has all this tents
+    # @helps.push(CellDontTouchTreeHelper.new)    # Help when a cell dont touch a tree, it will be a grass
+    # @helps.push(RowsAndColumnsFindTent.new)     # RowsAndColumnsFindTent help you to find Tent in a row or columns
+    # @helps.push(RowsAndColumnsFindGrass.new)    # RowsAndColumnsFindGrass help you to find grass in a row or columns
+    # @helps.push(CellWhiteOverlap.new)           # Help when all possibilities give a grass cell
+    @helps.push(AllTreesHaveTentsHelper.new)   # Place a tent for a tree that don't has his tent
 
   end
 
