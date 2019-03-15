8e751b732efb2011b22e0bba2d5615f840b26d9f
Lucien <lbouyeur@gmail.com>
2019-03-15 13:59:17 +0100

    RDoc générée

--- Game/Ui/Camille/Parametre.rb
+++ Game/Ui/Camille/Parametre.rb
@@ -17,7 +17,52 @@ def require_all(_dir)
 	}
 end
 
+##
+#==Presentation
+#Parametre is an inherited class from the Screen class. This class only needs
+#to be initialized and applied, so the only method is the initializer. It
+#creates and implements the language choice menu from the game by creating its
+#buttons.
+#
+#==Variables
+#The Parametre class creates the title size, text
+#size and the padding variables with :
+#     @pad =10
+#     @widthTitre=screen.width*0.10
+#     @heightTitre=screen.height*0.08
+#     @widthText=screen.width*0.08
+#     @heightText=screen.height*0.05
+#
+#This class also inherits the screen variable from the Screen class.
+#
+#The gtkObject is the table where the menu (a vertical box) is added to create
+#the menu buttons.
+#
+#==Implementation
+#=====Here is how the language choice is implemented on this screen :
+#
+#*First* we create the variables :
+#      fr ="Francais"
+#      ang="Anglais"
+#      langue=Gtk::ComboBoxText.new
+#
+#The langue variable is a combo box, making it easier for the player to choose.
+#
+#*Next* we add our combo box to the menu
+#      @menu.add(langue=Gtk::ComboBoxText.new)
+#
+#*Then* we add the action each possibility of the box does when it is clicked :
+#      langue.append_text(fr)
+#      langue.append_text(ang)
+#
+#Here we created a combo box with two possible languages, French and English.
+#The other buttons are created by doing this aswell.
+#
+#*Finally*, when all of the buttons are done we add a background picture with :
+#       @gtkObject.attach(Gtk::Image.new(pixbuf: @buffer),0,3,0,3)
+#
 class Parametre < Screen
+	#:nodoc:
   def initialize(manager)
 
     super(manager.win)
