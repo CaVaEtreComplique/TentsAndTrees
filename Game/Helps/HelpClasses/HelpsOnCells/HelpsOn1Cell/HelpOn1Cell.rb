2b9e34072b940b8326cb988a0d9fe1812194f233
Lucien <lbouyeur@gmail.com>
2019-03-15 11:57:43 +0100

    RDoc Générée

--- Game/Helps/HelpClasses/HelpsOnCells/HelpsOn1Cell/HelpOn1Cell.rb
+++ Game/Helps/HelpClasses/HelpsOnCells/HelpsOn1Cell/HelpOn1Cell.rb
@@ -5,12 +5,15 @@
 # @Last modified by:   Maxime
 # @Last modified time: 15-Mar-2019
 
-
-
 require File.dirname(__FILE__) + "/../HelpOnCells"
 
+##
+# The HelpOn1Cell class is an abstract class inherited from the HelpOnCells
+# class. It is a model for all the help classes that will only need to know one
+# cell. This class only knows the initialize method and it just calls the
+# initialize method from HelpOnCells with a cell in parameters.
 class HelpOn1Cell < HelpOnCells
-  #abstract class
+   #:nodoc:
   def initialize(cell)
     super(cell)
   end
