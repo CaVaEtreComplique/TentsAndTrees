fee0878a6ae667ec662f95112ef5744a60d23078
Lucien <lbouyeur@gmail.com>
2019-03-18 14:50:03 +0100

    RDoc de Grid générée

--- Game/Core/Grid.rb
+++ Game/Core/Grid.rb
@@ -5,17 +5,40 @@
 # @Last modified by:   zeigon
 # @Last modified time: 05-Mar-2019
 
-
-
 require File.dirname(__FILE__) + "/Cell"
 
+
+##
+# ====Presentation
+# The Grid class is the core of all the grids in the game.
+#
+# ====Variables
+# Each grid will know two variables : @rows and @cols : two array lists,
+# respectively the grid's rows and the grid's columns, one being the transposed
+# of the other.
+#
+# ====Methods
+# This class knows the initialization method and the methods described below.
 class Grid
 	@rows
 	@cols
 
+	#:nodoc:
 	attr_reader :rows, :cols
+	#:startdoc:
 
-	def initialize(nRow, nCol,gridAnswers,withAnswers=false)
+	# ====Parameters
+	# The initialization method has in parameters the number of rows,
+	# the number of columns, a grid and a boolean variable "withAnswers"
+	# being set to false by default. If the grid in third parameter has the
+	# answers, the withAnswers variable is set to true. The grid in third
+	# parameter is taken from a text file.
+	#
+	# ====Variables initialization
+	# In this method, the @rows list takes its values from the grid in parameters
+	# by reading the text file. When the @rows variable is initialized, the @cols
+	# variable can be initialized by just transposing the @rows variable.
+	def initialize(nRow, nCol, gridAnswers, withAnswers=false)
 		ProcessStatus.send("Initialisation de la grille de jeu")
 		@gridAnswers=gridAnswers
 		@rows = (0..nRow-1).map {|x|
@@ -34,6 +57,10 @@ class Grid
 		@cols = @rows.transpose
 	end
 
+	##
+	# The copyFrozen method creates a new grid that will have all the non-white
+	# cells frozen in order to save the unfrozen grid if the hypothesis is
+	# rejected.
 	def copyFrozen
 		newG = Grid.new(@rows.size, @cols.size,@gridAnswers)
 		@rows.zip(newG.rows).each {|oldRow, newRow|
@@ -44,14 +71,20 @@ class Grid
 		return newG
 	end
 
+	# The each method goes through the grid's rows.
 	def each
 		yield @rows.each
 	end
 
+	# The cellAt method returns the cell with the coordinates taken in
+	# parameters.
 	def cellAt(row, col)
 		return @rows[row][col]
 	end
 
+	# The == method compares two grids and returns true if the grid taken in
+	# parameters is the same as the grid we're testing. This method is especially
+	# useful to test if the player has finished the grid.
 	def ==(otherGrid)
 		@rows.each_with_index{ |cols,x|
 			cols.each_with_index { |_,y|
