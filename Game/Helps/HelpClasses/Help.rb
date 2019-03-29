1bdac4f961893fadb548588ef474eb981075c50d
Lucien <lbouyeur@gmail.com>
2019-03-29 14:07:53 +0100

    RDoc mise en forme, ARBORESCENCE À AJOUTER

--- Game/Helps/HelpClasses/Help.rb
+++ Game/Helps/HelpClasses/Help.rb
@@ -9,9 +9,12 @@
 
 require_all(__FILE__)
 ##
-#The help class is an abstract class which contains a String to explain the
-#help. It contains the methods helpFound? and getText and they act on the help
-#text. This abstract class is the parent of every help in the game.
+# The help class is an abstract class. It is the general parent of all the other
+# help classes in the game. To see the help classes tree, see below :
+#
+# *AJOUTER LE FICHIER D'ARBORESCENCES*
+#
+# This class knows the methods below :
 class Help
   # :nodoc:
   private_class_method :new  #abstract class
@@ -19,6 +22,9 @@ class Help
 
   # :startdoc:
   # The class' constructor.
+  # This method takes no parameters and initializes two variables :
+  # * +price+ : An integer giving the cost of each help. Here, it is initialized at 5.
+  # * +helpText+ : The text that will be shown every time the player needs help.
   def initialize()
     @price = 5
     #Initializes the help text as what it needs to be.
@@ -37,7 +43,8 @@ class Help
     return true
   end
 
-  # This method fetches the help text.
+  # This method fetches the help text for a given help level. It is used to
+  # initialize the @helpText variable in the class' constructor.
   # ===== Returns
   # Returns the help text.
   # ---------
@@ -45,13 +52,15 @@ class Help
     return "Txt not Available"
   end
 
-  # This method prints the help text.
+  # This method a redefinition of the to_s method, used to print the help text.
+  # ===== Returns
+  # Returns the @helpText variable's value and a line break.
   # -----------
   def to_s
     return @helpText + "\n"
   end
 
-  # This method returns the UI printable form of the help
+  # This method returns the UI printable form of the help.
   # -----------
   def getRes(helpLevel)
     res = Array.new
@@ -59,17 +68,27 @@ class Help
     res.push(cellsList(helpLevel))
   end
 
-  # This method returns an array with all the cells important for the help
+  # This method returns an array with all the important cells for the help.
   # -----------
   def cellsList(helpLevel)
     return Array.new
   end
 
-
+  # This method checks the price of a given help level.
+  # ===== Returns
+  #  @price * helpLevel
+  # The @price variable (initialized at 5) times the help level, so the prices
+  # go higher as the help gets higher and the game gets easier.
+  # ----------
   def price(helpLevel)
     return @price * helpLevel
   end
 
+  # This method is a redefinition of the == method.
+  # ===== Returns
+  #   return (self.class == otherHelp.class)
+  # A boolean telling if the two classes are equal.
+  # ----------
   def ==(otherHelp)
     return (self.class == otherHelp.class)
   end
