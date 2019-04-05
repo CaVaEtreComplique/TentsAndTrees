951596dc3b6d6356e01c9d26aa5b617f8993795c
camvde <camille.vaidie.72@gmail.com>
2019-04-05 15:07:49 +0200

    ajout refresh star

--- Game/Ui/Camille/Star.rb
+++ Game/Ui/Camille/Star.rb
@@ -17,26 +17,32 @@ end
 class Star
   attr_reader :stars
   def initialize(nbEtoile,nbEtoileObtenu)
-		sHappy=Asset.new(File.dirname(__FILE__) + "/../../../Assets/star.png")
-		sSad=Asset.new(File.dirname(__FILE__) + "/../../../Assets/starSad.png")
+		@sHappy=Asset.new(File.dirname(__FILE__) + "/../../../Assets/star.png")
+		@sSad=Asset.new(File.dirname(__FILE__) + "/../../../Assets/starSad.png")
     @stars=Gtk::Box.new(:horizontal,25)
-    (1.. nbEtoile).each { |e|
-       @star=Gtk::EventBox.new()
-       if(e <= nbEtoileObtenu)
-         #s=Asset.new(File.dirname(__FILE__) + "/../../../Assets/star.png")
-				 sH=sHappy
-				 sH.resize(75,75)
-	       sH.applyOn(@star)
-			 else
-         #s=Asset.new(File.dirname(__FILE__) + "/../../../Assets/starSad.png")
-				 sS=sSad
-				 sS.resize(75,75)
-	       sS.applyOn(@star)
-			 end
-    #   s.resize(50,50)
-    #   s.applyOn(@star)
-       @stars.add(@star)
-     }
+		refresh(nbEtoile,nbEtoileObtenu)
    end
-
+	 def refresh(nbEtoile,nbEtoileObtenu)
+		 @stars.each{|child|
+			 @stars.remove(child)
+		 }
+		 (1.. nbEtoile).each { |e|
+				@star=Gtk::EventBox.new()
+				if(e <= nbEtoileObtenu)
+					#s=Asset.new(File.dirname(__FILE__) + "/../../../Assets/star.png")
+				 sH= @sHappy
+				 sH.resize(75,75)
+				 sH.applyOn(@star)
+			 else
+					#s=Asset.new(File.dirname(__FILE__) + "/../../../Assets/starSad.png")
+				 sS=@sSad
+				 sS.resize(75,75)
+				 sS.applyOn(@star)
+			 end
+		 #   s.resize(50,50)
+		 #   s.applyOn(@star)
+				@stars.add(@star)
+			}
+			@stars.show_all
+	 end
 end
