1dc0e2bb69e66d3a2f2a8b61247a1aac9578ceb2
Maxime <titouf-72@hotmail.fr>
2019-04-05 11:20:27 +0200

    Création du screen

--- Game/Tutorial/TutorialUI.rb
+++
@@ -1,6 +0,0 @@
-# @Author: Maxime Touze <maxime>
-# @Date:   05-Apr-2019
-# @Email:  maxime_touze@univ-lemans.fr
-# @Filename: TutorialUI.rb
-# @Last modified by:   maxime
-# @Last modified time: 05-Apr-2019
