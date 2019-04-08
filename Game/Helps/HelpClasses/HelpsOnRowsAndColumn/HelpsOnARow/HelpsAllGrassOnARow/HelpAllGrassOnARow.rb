7333eeac17e478800a77732d9e7443589bc22270
Lucien <lbouyeur@gmail.com>
2019-04-08 15:02:20 +0200

    RDoc générée

--- Game/Helps/HelpClasses/HelpsOnRowsAndColumn/HelpsOnARow/HelpsAllGrassOnARow/HelpAllGrassOnARow.rb
+++ Game/Helps/HelpClasses/HelpsOnRowsAndColumn/HelpsOnARow/HelpsAllGrassOnARow/HelpAllGrassOnARow.rb
@@ -5,21 +5,62 @@
 # @Last modified by:   maxime
 # @Last modified time: 05-Apr-2019
 
-
-
 require File.dirname(__FILE__) + "/../HelpOneRow"
 
+##
+# ===== Presentation
+# The HelpAllGrassOnARow class is inherited from the HelpOneRow abstract
+# class. It is used when all the empty cells left on a row are tents so that
+# the player knows when all the grass is placed.
+#
+# ===== Variables
+# * +column+ : The column that has all its grass placed.
+#
+# ===== Methods
+# This class knows its constructor and the getText method. The constructor only
+# calls the parent class' constructor.
+#
+# ===== Examples
+# This is the class' constructor. As said before, it only calls the parent
+# constructor with the given parameters :
+#
+#   def initialize(column)
+#     super(column)
+#   end
 class HelpAllGrassOnARow < HelpOneRow
-  #help the player when there is a row with all the grass needed, so the player need to fill with tents
-  public_class_method :new #instanciable class
+
+  # :nodoc:
+  public_class_method :new
 
   def initialize(row)
     super(row)
   end
+  # :startdoc:
 
-  def getText(helpLevel) #allgrassonrow
-     #returns the text of the help, saying that "All tents need to be placed, so all the empty cells of the row ° are tents"
-
+  ##
+  # ===== Presentation
+  # This method fetches the text from the XML file to show the help text.
+  #
+  # ===== Parameters
+  # * +helpLevel+ : The help's importance. The higher the level, the better the advice.
+  #
+  # ===== Returns
+  # This method fetches the help text from the XML file and returns it depending
+  # on the help level.
+  #
+  # ===== Examples
+  # When the help level is at 1, this method fetches the text only without giving
+  # any variable's value :
+  #     @helpText = @textManager.getHelpsTexts("allgrassonrow", helpLevel, 0)
+  # This will send to the player the following help text :
+  #    "You have placed all of a row's grass"
+  #
+  # But when the help level is at 3, this method fetches the text and gives the
+  # row's value so that the player will know where to act :
+  #    @helpText = @textManager.getHelpsTexts("allgrassonrow", helpLevel, 0) + @row[0].column.to_s + @textManager.getHelpsTexts("allgrassonrow", helpLevel, 1)
+  # This will send to the player the following text, with r being the row :
+  #    "The remaining cells of the row r are tents because all of this row's grass has been placed"
+  def getText(helpLevel)
     case(helpLevel)
       when 1
         @helpText = @textManager.getHelpsTexts("allgrassonrow", helpLevel, 0)
