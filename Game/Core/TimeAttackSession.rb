d8aaa95d592d30cfff0d74e57f11d0d69fa8467a
Sckylle <florian.tourpe@gmail.com>
2019-04-26 17:46:25 +0200

    RDoc généré a vérifier

--- Game/Core/TimeAttackSession.rb
+++ Game/Core/TimeAttackSession.rb
@@ -7,29 +7,73 @@
 
 require File.dirname(__FILE__) + "/Session"
 
+
+#
+# ==== Presentation
+# The TimeAttackSession class is inherited from the class Session.
+#
+# ==== Variables
+# * +game+ - This variables represent a Game class
+#
+# * +gridPick+ - This variable represente the GridGenerator class.
+#
+# * +time+ - A Timer Class from Gtk3.
+#
+# * +score+ - An integer that represent the score.
+#
+# * +gameMode+ - The gameMode of Session.
+#
+# * +difficulty+ - The difficulty involve.
+#
+# ==== Methods
+# This class knows an initialization method described below.
 class TimeAttackSession < Session
 
 	attr_reader :game, :gridPick, :time, :score, :gameMode, :difficulty
 	# attr_writer :score
 
+	##
+	# ===== Presentation
+	# This class' constructor initializes the variable using the parental
+	# constructor of Session and so create a TimeAttackSession.
+	#
+	# ===== Attributes
+  # * +difficulty+ : the difficulty of the game .
+  # * +partOfAdventure+ : Boolean representing of this session is part of the
+	# adventure.
+  #
+	# ===== Exemple
+	#    session = TimeAttackSession.new(:hard, false)
 	def initialize(difficulty, partOfAdventure=false)
 		super(:timeAttack, difficulty, partOfAdventure)
 	end
 
+	##
+	# ===== Presentation
+	# This methode redefine the continuable? method of Session class in order to
+	# say this session can be continued and thus be save.
+	#
+	# ===== Exemple
+	#    session.continuable?()  #return true
 	def continuable?
 		true
 	end
 
+	##
+	# ===== Presentation
+	# This methode calculate the score of session by using @game.
+	#
 	def calculateScore
 		if @game
 			@score+=game.time
 		end
 	end
 
-	def isTutorial?
-		return false
-	end
-
+	##
+	# ===== Presentation
+	# This methode set the time of the timer by using the time affected to each
+	# grid.
+	#
 	def setTime
 		@time=@gridPick.associatedTimer
 	end
--- Game/Core/TutorialSession.rb
+++ Game/Core/TutorialSession.rb
@@ -1,3 +1,4 @@
+
 # @Author: Maxime Touze <maxime>
 # @Date:   05-Apr-2019
 # @Email:  maxime_touze@univ-lemans.fr
@@ -6,11 +7,37 @@
 # @Last modified time: 05-Apr-2019
 require File.dirname(__FILE__) + "/Session"
 
+#
+# ==== Presentation
+# The TutorialSession class is inherited from the class Session.
+#
+# ==== Variables
+# * +game+ - This variables represent a Game class
+#
+# * +gridPick+ - This variable represente the GridGenerator class.
+#
+# * +time+ - A Timer Class from Gtk3.
+#
+# * +score+ - An integer that represent the score.
+#
+# * +gameMode+ - The gameMode of Session.
+#
+# * +difficulty+ - The difficulty involve.
+#
+# ==== Methods
+# This class knows an initialization method described below.
 class TutorialSession < Session
 
 	attr_reader :game, :gridPick, :time, :score, :gameMode, :difficulty
 	# attr_writer :score
 
+	##
+	# ===== Presentation
+	# This class' constructor initializes the variable using the parental
+	# constructor of Session and so create a TutorialSession.
+	#
+	# ===== Exemple
+	#    session = TutorialSession.new()
 	def initialize
 		super(:tutorial, :tutorial, false)
 	end
@@ -19,14 +46,26 @@ class TutorialSession < Session
 		true
 	end
 
-	def calculateScore
-		return 42
-	end
+	# def calculateScore
+	# 	return 42
+	# end
 
+	##
+	# ===== Presentation
+	# This methode redefine the isTutorial? method of Session class in order to
+	# define this session as a tutorial
+	#
+	# ===== Exemple
+	#    session.isTutorial?()  #return true
 	def isTutorial?
 		return true
 	end
 
+	##
+	# ===== Presentation
+	# This methode set the timer at a random value in order to not make the
+	# tutorial bug.
+	#
 	def setTime
 		@time=42
 	end
