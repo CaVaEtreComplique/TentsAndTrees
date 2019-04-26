69221865184111161e028d279f425217edb3bed0
camvde <camille.vaidie.72@gmail.com>
2019-04-26 10:54:39 +0200

    RDoc a verif

--- Game/Ui/AssetsLoaderClass/IconAsset.rb
+++ Game/Ui/AssetsLoaderClass/IconAsset.rb
@@ -1,9 +1,22 @@
 
+# @Author: VAIDIE Camille <CamilleVAIDIE>
+# @Date:   25-Jan-2019
+# @Email:  camille.vaidie.etu@univ-lemans.fr
+# @Filename: ModeDeJeu.rb
+# @Last modified by:   vaidie Camille
+# @Last modified time: 08-Apr-2019
 
-
+##
+# ===== Presentation
+# Star only needs to be initialized.
+# It load all the icon.
+##
 class IconAsset
   attr_reader :iconAsset
   @iconAsset
+  ##
+  # ===== Presentation
+  # This method is used to load all the icon by putting them in a hash table @iconAsset
   def initialize
     pathToIcon=File.dirname(__FILE__) + "/../../../Assets/Icons/"
     @iconAsset= {
@@ -13,7 +26,8 @@ class IconAsset
       diapo1: Asset.new(File.dirname(__FILE__) + "/../../../Assets/regle/regle1.png"),
       diapo2: Asset.new(File.dirname(__FILE__) + "/../../../Assets/regle/regle2.png"),
       diapo3: Asset.new(File.dirname(__FILE__) + "/../../../Assets/regle/regle3.png"),
-      diapo4: Asset.new(File.dirname(__FILE__) + "/../../../Assets/regle/regle4.png")
+      diapo4: Asset.new(File.dirname(__FILE__) + "/../../../Assets/regle/regle4.png"),
+      quitter: Asset.new(File.dirname(__FILE__) + "/../../../Assets/Icons/cancel.png")
     }
   end
 end
