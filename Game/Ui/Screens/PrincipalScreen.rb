3f334d2882ea74ceacd2e02455e9f4efc2ff4c46
Lucien <lbouyeur@gmail.com>
2019-04-26 11:01:00 +0200

    RDoc générée

--- Game/Ui/Screens/PrincipalScreen.rb
+++ Game/Ui/Screens/PrincipalScreen.rb
@@ -15,48 +15,48 @@ require File.dirname(__FILE__) + "/../Buttons/Text"
 
 
 ##
-#==Presentation
-#FenetrePrinc is an inherited class from the Screen class. This class only needs
-#to be initialized and applied, so the only method is the initializer. It
-#creates and implements the main menu from the game by creating its buttons.
+# ===== Presentation
+# PrincipalScreen is an inherited class from the Screen class. This class only needs
+# to be initialized and applied, so the only method is the initializer. It
+# creates and implements the main menu from the game by creating its buttons.
 #
-#==Variables
-#The FenetrePrinc class creates the title size, text
-#size and the padding variables with :
+# ===== Variables
+# The PrincipalScreen class creates the title size, text
+# size and the padding variables with :
 #   @pad=30
 #   @widthTitre=screen.width*0.10
 #   @heightTitre=screen.height*0.08
 #   @widthText=screen.width*0.15
 #   @heightText=screen.height*0.04
 #
-#This class also inherits the screen variable from the Screen class.
+# This class also inherits the screen variable from the Screen class.
 #
-#The gtkObject is the table where the menuV (a vertical box) is added to create
-#the menu buttons.
+# The gtkObject is the table where the menuV (a vertical box) is added to create
+# the menu buttons.
 #
-#==Implementation
-#=====Here is how a button is implemented on the main menu :
+# ===== Implementation
+# Here is how a button is implemented on the main menu :
 #
-#*First* we add some text to the button :
+# *First* we add some text to the button :
 #   jouer=Text.new("jouer",@widthText,@heightText)
 #
-#*Next* we add the button we just created in the VBox (gtkObject) :
+# *Next* we add the button we just created in the VBox (gtkObject) :
 #   @menuV.pack_start(jouer.gtkObject ,expand: false, fill: true, padding: @pad)
 #
-#*Then* we add the action the button does when it is clicked :
+# *Then* we add the action the button does when it is clicked :
 #   jouer.onClick{
 #      manager.modeScreen.applyOn(@parent)
 #   }
-#Here we created a button entitled "jouer" that leads to the game mode menu
-#ModeDeJeu.
-#The other buttons are created by doing this aswell.
+# Here we created a button entitled "jouer" that leads to the game mode menu
+# GameModeScreen.
+# The other buttons are created by doing this aswell.
 #
-#*Finally*, when all of the buttons are done we add a background picture with :
+# *Finally*, when all of the buttons are done we add a background picture with :
 #   @gtkObject.attach(Gtk::Image.new(pixbuf: @buffer),0,3,0,3)
 #
 
 class PrincipalScreen < Screen
-  def initialize(manager) #:nodoc:
+  def initialize(manager) # :nodoc:
     super(manager.win)
     screen = Constants::SCREEN
 		@pad=Constants::BUTTON_PADDING
